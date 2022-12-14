a881c1d00fa8b23f835ca8c3cfb4897bab8d6eef
Nagalli Sreedhar <118349767+Sreedhar2484@users.noreply.github.com>
2022-12-14 19:57:19 +0530

    added searchperson in a city using java streams

---
+++ com/bridgelabz/addressbook/AddressBookIF.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.addressbook;
+
+import java.util.List;
+
+public interface AddressBookIF {
+    void operation();
+
+    void addContact();
+
+    void deletePerson();
+
+    void displayContents();
+
+    void editPerson();
+
+    void writeToAddressBookFile();
+
+    List<String> readDataFromFile();
+}
